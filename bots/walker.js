68002bf11bba016352b5a53dfc6b9a71c12675b7
greeve <greg_reeve@byu.edu>
2015-07-30 02:16:35 -0600

    Preprations for war.

--- bots/walker.js
+++ bots/walker.js
@@ -9,9 +9,11 @@ Walker.prototype = new Bot();
 
 Walker.prototype.setup = function() {
     this.timer = 0;
-    this.attrStrength = 10
+    this.target = undefined;
+    this.strafe = 75;
+    this.attrStrength = 8;
     this.safety = 10;
-    this.repStrength = 4;
+    this.repStrength = 2;
     this.commands = ['forward', 'backward', 'right', 'strafe-right', 'left', 'strafe-left', 'fire', 'mine', 'wait'];
 };
 
@@ -43,39 +45,147 @@ Walker.prototype.getTeamBots = function() {
 };
 
 Walker.prototype.getTarget = function() {
-    target = this.getBotById(this.state.payload.targets[this.id]);
-    if (target == undefined) {
-        opponents = this.getOpponentBots();
-        closeness = 5000;
-        for (i in opponents) {
-            distance = this.myDistanceToPoint(opponents[i].x, opponents[i].y);
-            if (distance < closeness) {
-                target = opponents[i];
-                closeness = distance;
-            }
-        }
+	var target = undefined;
+	var distance = 50000;	// Obscenely high number
+
+	for (i in this.state.bots) {
+		var bot = this.state.bots[i];
+		// Only attack opposing bots
+		if (bot.name != this.name) {
+			// If I'm closer to this bot, make it my target
+			if (this.myDistanceToPoint(bot.x, bot.y) < distance) {
+				target = bot;
+				this.state.payload.targets[this.id] = bot.id;
+				distance = this.myDistanceToPoint(bot.x, bot.y);
+			}
+		}
+	}
+	return target;
+}
+
+Walker.prototype.sideStep = function() {
+    var command = '';
+    if (this.strafe > 25) {
+        command = "strafe-left";
+    } else {
+        command = "strafe-right";
     }
-    this.state.payload.targets[this.id] = target.id;
-    return target;
-};
+
+    // Decrease strafe counter
+    this.strafe--;
+
+    if (this.strafe == 0) {
+        this.strafe = 100;
+    }
+
+    return command;
+}
+
+Walker.prototype.checkCollisions = function(point) {
+	var collision = false;
+	var type = '';
+	var object = undefined;
+
+	var obstacles = server.getObstacles();
+	var bots = server.getBots();
+
+	// Check bots
+	for (i in bots) {
+		bot = bots[i];
+		if (server.collisionBotWeapon(bot, point)) {
+			collision = true;
+			type = 'bot';
+			object = bot;
+			break;
+		}
+	}
+
+	// Check obstacles
+	if (!collision) {
+		for (i in obstacles) {
+			obstacle = obstacles[i];
+			if (server.collisionObstacle(obstacle, point)) {
+				collision = true;
+				type = 'obstacle';
+				object = obstacle;
+				break;
+			}
+		}
+	}
+
+	// Check boundaries
+	if (!collision && server.collisionBoundary(point)) {
+		collision = true;
+		type = 'boundary';
+		object = undefined;
+	}
+	
+	return { collision: collision, type: type, object: object };
+}
+
+Walker.prototype.pointInCircle = function(x, y, centerX, centerY, radius) {
+    var squareDistance = Math.pow(centerX - x, 2) + Math.pow(centerY - y, 2)
+    return squareDistance < Math.pow(radius, 2)
+}
+
+Walker.prototype.distanceToCollision = function(x, y, angle) {
+	var speed = 5;
+	var newPoint = { x: x, y: y };
+	var response = {};
+	response.collision = false;
+
+	while (!response.collision) {
+		newPoint = server.helpers.calcVector(newPoint.x, newPoint.y, angle, speed);
+		response = this.checkCollisions(newPoint);
+	}
+
+	distance = this.myDistanceToPoint(newPoint.x, newPoint.y);
+
+	return { distance: distance, type: response.type, object: response.object };
+}
 
 Walker.prototype.run = function() {
 	this.timer++;
     var command = '';
 
     if (typeof this.state.payload.targets == 'undefined') {
-        this.state.payload.targets = {}
+        this.state.payload.targets = {};
     }
 
     this.target = this.getTarget();
-    this.direction = this.getDirection(this.target, 0.05);
+    if (this.target != undefined) { 
+        this.direction = this.getDirection(this.target, 0.05);
+        var distToTarget = this.myDistanceToPoint(this.target.x, this.target.y);
+        var futureForwardPosition = server.helpers.calcVector(this.x, this.y, this.angle, this.radius + 1);
+        var nextCollision = this.distanceToCollision(futureForwardPosition.x, futureForwardPosition.y, this.angle);
+        var facingEnemy = (nextCollision.type == 'bot' && nextCollision.object.name != this.name);
+        var facingFriend = (nextCollision.type == 'bot' && nextCollision.object.name == this.name && nextCollision.distance < 50);
+        var facingObstacle = (nextCollision.type == 'obstacle' && nextCollision.distance < 50);
+        var facingBoundary = (nextCollision.type == 'boundary' && nextCollision.distance < 50);
 
-    command = '';
-	if (this.canShoot && this.weapons.bullet > 0) {
-        command = 'fire';
-    } else if (command != this.direction.command) {
-        command = this.direction.command;
-    }     
+        var tooFar = distToTarget > 50 * this.radius;
+
+        command = '';
+        if (this.canShoot && this.weapons.bullet > 0 && facingEnemy && !tooFar) {
+            command = 'fire';
+        } else if (command != 'forward') {
+            command = this.direction.command;
+        } else {
+            if (tooFar) {
+                command = "forward";
+            } else if (this.canShoot && this.weapons.bullet > 0) {
+                command = 'fire';
+            } else {
+                command = this.sideStep();
+            }
+        }
+
+        if (facingFriend || facingObstacle || facingBoundary) {
+            command = this.sideStep();
+        }
+    } else {
+        command = 'wait';
+    }
 
     return { command: command, team: this.state.payload };
 };
